95f0f4099cf61269b1118faf16cfa9288d2b7c68
shreyanshshandilya15 <shreytiwari56@gmail.com>
2024-02-23 17:48:13 +0530

    fixed commit

--- data/database.js
+++ data/database.js
@@ -4,6 +4,6 @@ export const connectDB=()=>{
     mongoose.connect(process.env.MONGO_URL,{
     dbName:"backendapi"
 })
-.then(()=>console.log("database connected!"))
+.then((c)=>console.log(`database connected with ${c.connection.host}`))
 .catch((e)=>console.log(e));
 };
